0255558e9436a61d4ca725f2e3d14f73446d5eea
wen <harry82111@gmail.com>
2020-04-26 02:36:12 +0800

    新增查詢會員資料

---
+++ product-core/src/main/java/team/takoyaki/vo/MemberInfoResp.java
@@ -0,0 +1,53 @@
+package team.takoyaki.vo;
+
+import java.sql.Timestamp;
+
+import lombok.Data;
+
+@Data
+public class MemberInfoResp {
+
+	private String name;
+
+	private String nickname;
+
+	private String mobilePhone;
+
+	private String telephone;
+
+	private String email;
+
+	private String memberLevel;
+
+	private String memberType;
+
+	private Timestamp joinDatetime;
+
+	private Timestamp updateDatetime;
+
+	private String country;
+
+	private String city;
+
+	private String district;
+
+	private String township;
+
+	private String village;
+
+	private String neighborhood;
+
+	private String street;
+
+	private String road;
+
+	private String section;
+
+	private String lane;
+
+	private String alley;
+
+	private String number;
+
+	private Integer floor;
+}
