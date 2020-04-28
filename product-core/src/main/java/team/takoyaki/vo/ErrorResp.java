daf21d9678373c310c1bed3a1c766e0539beee54
wen <harry82111@gmail.com>
2020-04-29 01:11:57 +0800

    新增error resp

--- product-core/src/main/java/team/takoyaki/vo/ErrorResp.java
+++ product-core/src/main/java/team/takoyaki/vo/ErrorResp.java
@@ -2,11 +2,14 @@ package team.takoyaki.vo;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import lombok.Data;
+
+@Data
 public class ErrorResp {
-	
+
 	@JsonProperty(value = "ERROR_CODE")
 	private String errorCode;
-	
+
 	@JsonProperty(value = "MESSAGE")
 	private String message;
 }
